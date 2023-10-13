c0fce5fc6fdf7e255e1d4452733942503485c1d3
Jáchym <Jchym@DESKTOP-7TE5MQ4>
2023-10-13 08:44:08 +0200

    Přidejte soubory projektu.

---
+++ ProjektakNuker/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProjektakNuker
+{
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting browser...");
+            IWebDriver driver = new ChromeDriver();
+            Console.WriteLine("Opening page...");
+            driver.Navigate().GoToUrl("https://poll.ly/qdSK0ZQlirguTNNBxnCg");
+
+        }
+    }
+}
