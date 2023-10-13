e3a535bb2f2a29cd210881bed8f07b3eb677172a
rezackacz <44003501+rezackacz@users.noreply.github.com>
2023-10-13 09:58:31 +0200

    Added a few somethings...

--- ProjektakNuker/Program.cs
+++ ProjektakNuker/Program.cs
@@ -1,16 +1,24 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace ProjektakNuker
-{
-    internal class Program {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Starting browser...");
-            IWebDriver driver = new ChromeDriver();
-            Console.WriteLine("Opening page...");
-            driver.Navigate().GoToUrl("https://poll.ly/qdSK0ZQlirguTNNBxnCg");
+namespace ProjektakNuker {
+  internal class Program {
+    static void Main(string[] args) {
 
-        }
+
+
+      Console.WriteLine("Starting browser...");
+      IWebDriver driver = new ChromeDriver();
+
+      Console.WriteLine("Opening page...");
+      driver.Navigate().GoToUrl("https://poll.ly/qdSK0ZQlirguTNNBxnCg");
+      driver.Manage().Cookies.DeleteAllCookies();
+      driver.Navigate().Refresh();
+
+      Console.WriteLine("Page is up'n'running, proceeding to destroy the fucking cookie dialog...");
+
+
+
     }
+  }
 }
